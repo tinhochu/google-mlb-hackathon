31882923a78c2521bc1f0ecb02928e044c37d907
Tin Ho Chu <mchu.tinho@gmail.com>
2024-12-28 00:29:56 -0500

    Changes on the Search Player Fn
    Signed-off-by: Tin Ho Chu <mchu.tinho@gmail.com>

--- functions/SearchPlayers/src/main.js
+++ functions/SearchPlayers/src/main.js
@@ -1,35 +1,44 @@
-import { Client, Users } from 'node-appwrite';
-
-// This Appwrite function will be executed every time your function is triggered
+import { gotScraping } from 'got-scraping';
+/**
+ * * This function will be executed every time your function is triggered
+ * * It will search for players in the MLB Algolia Service
+ * * It will return the players that match the search query
+ */
 export default async ({ req, res, log, error }) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
-  const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
-
   try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
-  }
+    // Check if its POST request
+    if (req.method !== 'POST') {
+      return res.json({ error: 'Method not allowed' }, 405);
+    }
 
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
+    // Get the body
+    const body = req.body;
 
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
+    // Find in the MLB API
+    const response = await gotScraping.get(
+      `${process.env.MLB_API_ENDPOINT}/v1/people/search`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        searchParams: {
+          names: body.query,
+        },
+      }
+    );
+
+    // Parse the response
+    const data = JSON.parse(response.body);
+
+    // Return the players
+    return res.json({
+      data: {
+        players: data.people,
+      },
+    });
+  } catch (e) {
+    error(e);
+
+    return res.json({ error: 'Internal server error' }, 500);
+  }
 };
